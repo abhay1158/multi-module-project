48cff7f64bfc1abbb706c709c40d0a6bc6f335dc
Abhay Pratap Singh <abhayp.singh200029@gmail.com>
2025-06-02 12:16:38 +0530

    Module b changes

--- module-b/src/main/java/com/example/moduleb/controller/ModuleBController.java
+++ module-b/src/main/java/com/example/moduleb/controller/ModuleBController.java
@@ -11,9 +11,13 @@ public class ModuleBController {
     @Autowired
     private RestTemplate restTemplate;
 
-    @GetMapping("/module-b")
-    public String getMessageFromModuleB() {
+    @GetMapping("/module-a/from-module-b")
+    public String getMessageOfModuleAFromModuleB() {
         String moduleAResponse = restTemplate.getForObject("http://localhost:8081/module-a", String.class);
         return "Message from Module B: " + moduleAResponse;
+    }
+    @GetMapping("/module-b")
+    public String getMessageFromModuleB() {
+        return "Hello from Module B 🙋‍♂️";
     }
 }
